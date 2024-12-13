e320367cad2ad09e46f0a1671a3b8754db04ec56
Degareddy <dega99@gmail.com>
2024-12-13 10:41:51 +0300

    env changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  envName: 'local',
+   Url: 'https://degatech.in/dme/api/',
+  //Url: 'http://183.83.32.116:5000/api/'
+
+   //Url:'https://localhost:5000/api/'
+};
+
+
+// ng build --configuration=production --base-href=/dega/
