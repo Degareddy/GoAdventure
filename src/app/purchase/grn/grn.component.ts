4c53baa146d3b6c039f1f7cf6385046a586be483
pratapgundla <prathapgundla@gmail.com>
2025-01-12 11:07:48 +0530

    grn and purchase

--- src/app/purchase/grn/grn.component.ts
+++ src/app/purchase/grn/grn.component.ts
@@ -120,7 +120,7 @@ export class GrnComponent implements OnInit, OnDestroy {
     const curbody = {
       ...this.commonParams(),
       Item: "CURRENCY",
-      mode:this.grnForm.get('mode')?.value
+      mode: this.grnForm.get('mode')?.value
     };
     const service1 = this.invService.getModesList(modebody);
     const service2 = this.invService.GetMasterItemsList(curbody);
@@ -226,28 +226,19 @@ export class GrnComponent implements OnInit, OnDestroy {
     }
     this.subSink.sink = this.purchaseService.GetTranCount(body).subscribe((res: any) => {
       if (res.status.toUpperCase() != "FAIL" && res.status.toUpperCase() != "ERROR" && res.status.toUpperCase() != "FAILED") {
-
-
-        if (res.data.tranCount === 1) {
-
-
+        if (res && res.data && res.data.tranCount === 1) {
           this.masterParams.tranNo = res.data.selTranNo;
           this.getGRNData(this.masterParams, this.grnForm.controls['mode'].value);
         }
         else {
           if (!this.dialogOpen) {
-
-
             const dialogRef: MatDialogRef<SearchEngineComponent> = this.dialog.open(SearchEngineComponent, {
               width: '90%',
               disableClose: true,
               data: { tranNum: this.grnForm.controls['tranNo'].value, search: 'GRN Search', TranType: "GRN" }  // Pass any data you want to send to CustomerDetailsComponent
             });
             dialogRef.afterClosed().subscribe(result => {
-
               if (result != true && result != undefined) {
-
-
                 this.dialogOpen = false;
                 this.masterParams.tranNo = result;
                 this.getGRNData(this.masterParams, this.grnForm.controls['mode'].value);
@@ -255,6 +246,21 @@ export class GrnComponent implements OnInit, OnDestroy {
             });
           }
 
+        }
+      } else {
+        if (!this.dialogOpen) {
+          const dialogRef: MatDialogRef<SearchEngineComponent> = this.dialog.open(SearchEngineComponent, {
+            width: '90%',
+            disableClose: true,
+            data: { tranNum: this.grnForm.controls['tranNo'].value, search: 'GRN Search', TranType: "GRN" }  // Pass any data you want to send to CustomerDetailsComponent
+          });
+          dialogRef.afterClosed().subscribe(result => {
+            if (result != true && result != undefined) {
+              this.dialogOpen = false;
+              this.masterParams.tranNo = result;
+              this.getGRNData(this.masterParams, this.grnForm.controls['mode'].value);
+            }
+          });
         }
       }
 
@@ -295,7 +301,7 @@ export class GrnComponent implements OnInit, OnDestroy {
       this.loader.start();
       this.subSink.sink = this.purchaseService.getGrnData(masterParams).subscribe((res: any) => {
         this.loader.stop();
-        if (res.status.toUpperCase() === "FAIL" || res.status.toUpperCase()==="ERROR") {
+        if (res.status.toUpperCase() === "FAIL" || res.status.toUpperCase() === "ERROR") {
           this.retMessage = res.message;
           this.textMessageClass = "red";
         }
@@ -324,7 +330,7 @@ export class GrnComponent implements OnInit, OnDestroy {
       width: '90%', // Set the width of the dialog
       disableClose: true,
       data: {
-        mode: this.grnForm.controls['mode'].value, status: this.grnStatus,vat:this.grnForm.controls['isVatable'].value,
+        mode: this.grnForm.controls['mode'].value, status: this.grnStatus, vat: this.grnForm.controls['isVatable'].value,
         tranNum: this.grnForm.controls['tranNo'].value, search: 'GRN Search', TranType: "GRN"
       }
     });
@@ -332,7 +338,7 @@ export class GrnComponent implements OnInit, OnDestroy {
       if (result.isAltered === true) {
         this.dialogOpen = false;
         this.masterParams.tranNo = this.grnForm.controls['tranNo'].value;
-        this.newTranMsg="Modified Successfully";
+        this.newTranMsg = "Modified Successfully";
         this.getGRNData(this.masterParams, this.grnForm.controls['mode'].value);
       }
     });
--- src/app/sales/customer/vendor-products/vendor-products.component.ts
+++ src/app/sales/customer/vendor-products/vendor-products.component.ts
@@ -9,7 +9,6 @@ import { SubSink } from 'subsink';
 import { vendorProducts } from '../../sales.class';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { CustomerService } from 'src/app/Services/customer.service';
-import { min } from 'moment';
 import { ColumnApi, GridApi, GridOptions } from 'ag-grid-community';
 interface Item {
   itemCode: string;
@@ -20,41 +19,38 @@ interface Item {
   templateUrl: './vendor-products.component.html',
   styleUrls: ['./vendor-products.component.css']
 })
-export class VendorProductsComponent implements OnInit,OnDestroy {
+export class VendorProductsComponent implements OnInit, OnDestroy {
   @Input() Mode!: string;
   @Input() custID!: string;
   retMessage: string = '';
   public rowSelection: 'single' | 'multiple' = 'multiple';
   public gridOptions!: GridOptions;
-
   previousTabIndex!: number;
   public selectedTabIndex: number = 0;
   retNum!: number;
-
   public serialNum!: number;
   public custId!: string;
-  public prodCode!:string;
+  public prodCode!: string;
   rowData: any = [];
   @Input() custName!: string;
   textMessageClass!: string;
- private columnApi!: ColumnApi;
+  private columnApi!: ColumnApi;
   private gridApi!: GridApi;
-  prodclass!:vendorProducts;
-
+  prodclass!: vendorProducts;
   columnDefs: any = [];
-  modes:Item[]=[
-    {itemCode:'Select',itemName:'Select'},
-    {itemCode:'Open',itemName:'Open'},
-    {itemCode:'Delete',itemName:'Delete'},
+  modes: Item[] = [
+    { itemCode: 'Select', itemName: 'Select' },
+    { itemCode: 'Open', itemName: 'Open' },
+    { itemCode: 'Delete', itemName: 'Delete' },
   ];
   vendorProductForm!: FormGroup;
   subSink: SubSink;
 
-  constructor(private fb: FormBuilder,protected custService: CustomerService, private userDataService: UserDataService,private loader: NgxUiLoaderService, private utlService: UtilitiesService,public dialog: MatDialog,) {
-        this.subSink = new SubSink();
-        this.vendorProductForm = this.formInit();
-    
-   }
+  constructor(private fb: FormBuilder, protected custService: CustomerService, private userDataService: UserDataService, private loader: NgxUiLoaderService, private utlService: UtilitiesService, public dialog: MatDialog,) {
+    this.subSink = new SubSink();
+    this.vendorProductForm = this.formInit();
+    this.prodclass = new vendorProducts();
+  }
   ngOnDestroy(): void {
     this.subSink.unsubscribe();
   }
@@ -65,13 +61,13 @@ export class VendorProductsComponent implements OnInit,OnDestroy {
   formInit() {
     return this.fb.group({
       productGroup: [''],
-       effectiveDate: [new Date(), [Validators.required]],
-       rate:[0,[Validators.required,]],
-       code:['',[Validators.required]],
-       status:['Select',[Validators.required]]
+      effectiveDate: [new Date(), [Validators.required]],
+      rate: [0, [Validators.required,]],
+      code: ['', [Validators.required]],
+      status: ['Select', [Validators.required]]
     });
   }
-  clear(){
+  clear() {
     this.formInit();
   }
   ngAfterViewInit(): void {
@@ -80,8 +76,8 @@ export class VendorProductsComponent implements OnInit,OnDestroy {
       { field: "prodName", headerName: "Product", sortable: true, filter: true, resizable: true, flex: 1 },
       { field: "prodStatus", headerName: "Status", sortable: true, filter: true, resizable: true, flex: 1 },
       { field: "prodStatus", headerName: "Status", sortable: true, filter: true, resizable: true, flex: 1 },
-     
-  
+
+
       {
         headerName: "Price",
         field: "rate",
@@ -94,7 +90,7 @@ export class VendorProductsComponent implements OnInit,OnDestroy {
         valueFormatter: (params: { data: { rate: any; }; }) => {
           const amount = params.data.rate;
           if (amount !== undefined) {
-            const formattedBalance=Math.abs(amount).toLocaleString();
+            const formattedBalance = Math.abs(amount).toLocaleString();
             // const formattedBalance = amount < 0
             //   ? `(${Math.abs(amount).toLocaleString()})`
             //   : amount.toLocaleString();
@@ -119,55 +115,55 @@ export class VendorProductsComponent implements OnInit,OnDestroy {
       },
     ]
   }
-//  onTabChanged(event: any) {
-//     const currentIndex = event.index;
-//     this.selectedTabIndex = currentIndex;
-//     if (currentIndex === 1 && this.previousTabIndex === 2) {
-//     } else if (currentIndex === 0 && this.previousTabIndex === 1) {
-//     } else {
+  //  onTabChanged(event: any) {
+  //     const currentIndex = event.index;
+  //     this.selectedTabIndex = currentIndex;
+  //     if (currentIndex === 1 && this.previousTabIndex === 2) {
+  //     } else if (currentIndex === 0 && this.previousTabIndex === 1) {
+  //     } else {
 
-//       if (currentIndex === 1 && (this.serialNum != 0 && this.serialNum != undefined) && (this.custId != undefined && this.custId != null)) {
-//         // this.contacts.refresh(this.custId, this.serialNum, this.custForm.controls['mode'].value);
-//       }
-//     }
-//     this.previousTabIndex = currentIndex;
-//   }
-  submit(){
+  //       if (currentIndex === 1 && (this.serialNum != 0 && this.serialNum != undefined) && (this.custId != undefined && this.custId != null)) {
+  //         // this.contacts.refresh(this.custId, this.serialNum, this.custForm.controls['mode'].value);
+  //       }
+  //     }
+  //     this.previousTabIndex = currentIndex;
+  //   }
+  submit() {
     this.prepareclass();
     this.loader.start();
-        this.subSink.sink = this.custService.UpdateSupplierProducts(this.prodclass).subscribe((result: SaveApiResponse) => {
-          this.loader.stop();
-          this.retMessage = result.message;
-          this.retNum = result.retVal;
-          if (this.retNum === 101 || this.retNum === 102 || this.retNum === 103 || this.retNum === 104) {
-            this.textMessageClass = "green"
-            this.retMessage = result.message;
-            this.loadData();
-            
-          }
-    
-          else if (this.retNum < 100) {
-            this.textMessageClass = "red"
-            this.retMessage = result.message;
-          }
-          else if (this.retNum > 200) {
-            this.textMessageClass = "red"
-            this.retMessage = result.message;
-          }
-          else {
-            this.textMessageClass = "red"
-            this.retMessage = result.message;
-          }
-        });
+    this.subSink.sink = this.custService.UpdateSupplierProducts(this.prodclass).subscribe((result: SaveApiResponse) => {
+      this.loader.stop();
+      this.retMessage = result.message;
+      this.retNum = result.retVal;
+      if (this.retNum === 101 || this.retNum === 102 || this.retNum === 103 || this.retNum === 104) {
+        this.textMessageClass = "green"
+        this.retMessage = result.message;
+        this.loadData();
+
+      }
+
+      else if (this.retNum < 100) {
+        this.textMessageClass = "red"
+        this.retMessage = result.message;
+      }
+      else if (this.retNum > 200) {
+        this.textMessageClass = "red"
+        this.retMessage = result.message;
+      }
+      else {
+        this.textMessageClass = "red"
+        this.retMessage = result.message;
+      }
+    });
 
   }
-  onRowSelected(event:any){   
-     console.log(event);
-     this.vendorProductForm.get('productGroup')?.patchValue(event.data.prodName);
-     this.vendorProductForm.get('rate')?.patchValue(event.data.rate);
-     this.vendorProductForm.get('code')?.patchValue(event.data.prodCode);
-     this.vendorProductForm.get('effectiveDate')?.patchValue(event.data.validUntil);
-     this.vendorProductForm.get('status')?.patchValue(event.data.prodStatus);
+  onRowSelected(event: any) {
+    console.log(event);
+    this.vendorProductForm.get('productGroup')?.patchValue(event.data.prodName);
+    this.vendorProductForm.get('rate')?.patchValue(event.data.rate);
+    this.vendorProductForm.get('code')?.patchValue(event.data.prodCode);
+    this.vendorProductForm.get('effectiveDate')?.patchValue(event.data.validUntil);
+    this.vendorProductForm.get('status')?.patchValue(event.data.prodStatus);
 
   }
   onGridReady(params: any) {
@@ -175,20 +171,19 @@ export class VendorProductsComponent implements OnInit,OnDestroy {
     this.columnApi = params.columnApi;
     this.gridApi.addEventListener('rowClicked', this.onRowSelected.bind(this));
   }
-  
-  prepareclass(){
-    this.prodclass=new vendorProducts();
-    this.prodclass.Mode=this.Mode;
-    this.prodclass.company=this.userDataService.userData.company;
-    this.prodclass.location=this.userDataService.userData.location;
-    this.prodclass.Supplier=this.custID;
-    this.prodclass.SlNo=0;
-    this.prodclass.ProdCode=this.vendorProductForm.controls['code'].value;
-    this.prodclass.Rate=this.vendorProductForm.controls['rate'].value;
-    this.prodclass.ValidUntil=this.vendorProductForm.controls['effectiveDate'].value;
-    this.prodclass.ProdStatus=this.vendorProductForm.controls['status'].value;
-    this.prodclass.User=this.userDataService.userData.userID;
-    this.prodclass.RefNo=this.userDataService.userData.sessionID;
+
+  prepareclass() {
+    this.prodclass.Mode = this.Mode;
+    this.prodclass.company = this.userDataService.userData.company;
+    this.prodclass.location = this.userDataService.userData.location;
+    this.prodclass.Supplier = this.custID;
+    this.prodclass.SlNo = 0;
+    this.prodclass.ProdCode = this.vendorProductForm.controls['code'].value;
+    this.prodclass.Rate = this.vendorProductForm.controls['rate'].value;
+    this.prodclass.ValidUntil = this.vendorProductForm.controls['effectiveDate'].value;
+    this.prodclass.ProdStatus = this.vendorProductForm.controls['status'].value;
+    this.prodclass.User = this.userDataService.userData.userID;
+    this.prodclass.RefNo = this.userDataService.userData.sessionID;
   }
   commonPrams() {
     return {
@@ -198,24 +193,24 @@ export class VendorProductsComponent implements OnInit,OnDestroy {
       user: this.userDataService.userData.userID
     }
   }
-  
+
   loadData() {
 
     if (this.custID) {
-      const body={
+      const body = {
         Company: this.userDataService.userData.company,
         Location: this.userDataService.userData.location,
         refNo: this.userDataService.userData.sessionID,
         user: this.userDataService.userData.userID,
         Item: this.custID
       }
-    
+
       try {
         this.loader.start();
         this.subSink.sink = this.custService.GetSupplierProducts(body).subscribe((res: any) => {
           this.loader.stop();
           if (res.status.toUpperCase() === "SUCCESS") {
-            this.rowData=res['data'];
+            this.rowData = res['data'];
             // this.rowData = this.processRowPostCreate(res['data']);
             this.displayMessage("Success: " + res.message, "green");
           }
@@ -238,7 +233,7 @@ export class VendorProductsComponent implements OnInit,OnDestroy {
     this.retMessage = message;
     this.textMessageClass = cssClass;
   }
-onProductSearch() {
+  onProductSearch() {
     const body = {
       ...this.commonPrams(),
       Type: "PRODUCT",
@@ -251,7 +246,6 @@ onProductSearch() {
         if (res && res.data && res.data.nameCount === 1) {
           this.vendorProductForm.controls['productGroup'].patchValue(res.data.selName);
           this.vendorProductForm.controls['code'].patchValue(res.data.selCode);
-          // this.productChange(res.data.selCode, this.productForm.get('mode')?.value);
         }
         else {
           const dialogRef: MatDialogRef<SearchProductComponent> = this.dialog.open(SearchProductComponent, {
@@ -263,16 +257,13 @@ onProductSearch() {
             }
           });
           dialogRef.afterClosed().subscribe(result => {
-            this.vendorProductForm.controls['productGroup'].patchValue(result.prodName);
-            this.vendorProductForm.controls['code'].patchValue(result.prodCode);
-            // this.vendorProductForm(result.prodCode, this.productForm.get('mode')?.value);
+            if(result){
+              this.vendorProductForm.controls['productGroup'].patchValue(result.prodName);
+              this.vendorProductForm.controls['code'].patchValue(result.prodCode);
+            }
           });
         }
       }
-      else {
-        // this.retMessage = res.message;
-        // this.textMessageClass = "red";
-      }
     });
-      }
+  }
 }
