1348b418a91d3bb39904cfa598793b7e00e840c8
degaharshithreddy <degaharshith3@gmail.com>
2025-04-09 10:42:19 +0530

    ot register search

--- src/app/payroll/payroll/ot-register/ot-register.component.ts
+++ src/app/payroll/payroll/ot-register/ot-register.component.ts
@@ -14,6 +14,10 @@ import { getPayload } from 'src/app/general/Interface/admin/admin';
 import { Item } from 'src/app/general/Interface/interface';
 import { Router } from '@angular/router';
 import { MasterParams } from 'src/app/modals/masters.modal';
+import { DatePipe } from '@angular/common';
+import { displayMsg, Mode, TextClr, TranStatus, Type } from 'src/app/utils/enums';
+import { AccessSettings } from 'src/app/utils/access';
+import { SearchEngineComponent } from 'src/app/general/search-engine/search-engine.component';
 
 @Component({
   selector: 'app-ot-register',
@@ -29,6 +33,7 @@ export class OtRegisterComponent implements OnInit, OnDestroy {
   retMessage: string = "";
   status!: string;
   private subSink!: SubSink;
+  detdialogOpen = false;
 
 
   public disableDetail: boolean = true;
@@ -36,9 +41,10 @@ export class OtRegisterComponent implements OnInit, OnDestroy {
   slNo:string=''
   @Input() max: any;
   tomorrow = new Date();
+  tranStatus!: string;
   public fetchStatus: boolean = true;
   constructor(private fb: FormBuilder, public dialog: MatDialog, private masterService: MastersService,
-     protected router: Router,
+     protected router: Router,private datePipe: DatePipe,
     private loader: NgxUiLoaderService, private payService: PayrollService, private userDataService: UserDataService) {
     this.otrForm = this.formInit();
     this.subSink = new SubSink();
@@ -164,23 +170,91 @@ export class OtRegisterComponent implements OnInit, OnDestroy {
   }
 
   get() {
-    const body = {
-      ...this.commonParams(),
-      TranNo: this.otrForm.controls['tranNo'].value,
-    };
-    this.loader.start();
-    this.subSink.sink = this.payService.GetOTRegister(body).subscribe((res: any) => {
-      this.loader.stop();
-      if (res.status.toUpperCase() === "SUCCESS") {
-        this.textMessageClass = "green";
-        this.retMessage = res.message;
-        this.otrForm.patchValue(res.data);
+    this.displayMessage("", "");
+        // this.OTREGISTERForm = this.formInit();
+        
+        const currentDate = new Date();
+        const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+        const formattedFirstDayOfMonth = this.formatDate(firstDayOfMonth);
+        const formattedCurrentDate = this.formatDate(currentDate);
+        const body = {
+          Company: this.userDataService.userData.company,
+          Location: this.userDataService.userData.location,
+          TranType: Type.OTREGISTER,
+          TranNo: this.otrForm.controls['tranNo'].value,
+          Party: "",
+          FromDate: formattedFirstDayOfMonth,
+          ToDate: formattedCurrentDate,
+          TranStatus: TranStatus.ANY,
+          User: this.userDataService.userData.userID,
+          RefNo: this.userDataService.userData.sessionID
+        }
+        this.subSink.sink = this.payService.GetTranCount(body).subscribe((res: any) => {
+          if (res.retVal === 0) {
+            if (res && res.data && res.data.tranCount === 1) {
+              this.masterParams.tranNo = res.data.selTranNo;
+              this.getOtRegData(this.masterParams, this.otrForm.controls['mode'].value);
+            }
+            else {
+              this.openSearch();
+            }
+          }
+          else {
+            this.openSearch();
+          }
+        });
+  }
+  getOtRegData(masterParams: MasterParams, mode: string) {
+      this.loader.start();
+      try {
+        this.subSink.sink = this.payService.GetOTRegister(masterParams).subscribe((res: any) => {
+          this.loader.stop();
+          if (res.status.toUpperCase() == AccessSettings.SUCCESS) {
+            // this.expCls = res['data'];
+            this.tranStatus = res['data'].tranStatus;
+            this.otrForm.controls['tranNo'].patchValue(res['data'].tranNo);
+            
+            this.textMessageClass = 'green';
+            if (mode.toUpperCase() != Mode.view) {
+              this.retMessage = res.message;
+            }
+            else {
+              this.retMessage = 'Retriving data ' + res.message + ' has completed';
+            }
+          }
+          else {
+            this.displayMessage(displayMsg.ERROR + res.message, TextClr.red);
+          }
+        });
       }
-      else{
-        this.textMessageClass = "red";
-        this.retMessage = res.message;
+      catch (ex: any) {
+        this.displayMessage(displayMsg.EXCEPTION + ex.message, TextClr.red);
       }
-    });
+    }
+    openSearch() {
+        if (!this.detdialogOpen) {
+          const dialogRef: MatDialogRef<SearchEngineComponent> = this.dialog.open(SearchEngineComponent, {
+            width: '90%',
+            disableClose: true,
+            data: {
+              tranNum: this.otrForm.get('tranNo')?.value,
+              search: 'OTREGISTER Search', TranType: Type.OTREGISTER
+            }
+          });
+          this.detdialogOpen = true;
+          dialogRef.afterClosed().subscribe(result => {
+            this.detdialogOpen = false;
+            if (result != true && result != undefined) {
+              // console.log(result);
+              this.masterParams.tranNo = result;
+              this.getOtRegData(this.masterParams, this.otrForm.controls['mode'].value);
+            }
+    
+          });
+        }
+      }
+  formatDate(date: Date): string {
+    return this.datePipe.transform(date, 'yyyy-MM-dd') || '';
   }
   onDetailsCilcked(value: string) {
 
@@ -197,6 +271,10 @@ export class OtRegisterComponent implements OnInit, OnDestroy {
     this.otrForm = this.formInit();
     this.textMessageClass="";
     this.retMessage="";
+  }
+  private displayMessage(message: string, cssClass: string) {
+    this.retMessage = message;
+    this.textMessageClass = cssClass;
   }
 
   onDocsCilcked(value: string) {
