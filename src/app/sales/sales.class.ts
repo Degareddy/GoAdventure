b131bcd0d31935bcbc269f4dd7a8e73bc508823f
DegaHariniReddy <harinidega4@gmail.com>
2025-05-14 17:39:27 +0530

    date issue in trips

--- src/app/sales/sales.class.ts
+++ src/app/sales/sales.class.ts
@@ -9,6 +9,25 @@ export class dateFormat{
     return this.datepipe.transform(date, 'yyyy-MM-dd') || '';
   }
 }
+export class addOneDay{
+  constructor(    private datepipe: DatePipe,
+  ){
+    
+  }
+  public formatDate(date: Date): string {
+    if (!date) return '';
+    else if(date == new Date()){
+          return this.datepipe.transform(date, 'yyyy-MM-dd') || '';
+
+    }
+
+    // Add 1 day to correct for timezone offset
+    // const correctedDate = new Date(date);
+    // correctedDate.setDate(correctedDate.getDate() + 1);
+
+    return this.datepipe.transform(date, 'yyyy-MM-dd') || '';
+  }
+}
   export class Customer {
   public mode!: string
   public company!: string
