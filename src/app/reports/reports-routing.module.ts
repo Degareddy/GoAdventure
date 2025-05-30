aac5a08a4f9dc1bb04b063a7e5e091ef42b9886d
Degareddy <dega99@gmail.com>
2025-05-30 15:39:59 +0530

    bbh

--- src/app/reports/reports-routing.module.ts
+++ src/app/reports/reports-routing.module.ts
@@ -5,13 +5,11 @@ import { ReportGuard } from './_guard/report.guard';
 import { PlreportsComponent } from './plreports/plreports.component';
 import { CashBalancesComponent } from './cash-balances/cash-balances.component';
 import { PurchaseStatementsComponent } from '../purchase/purchase-statements/purchase-statements.component';
-import { TripProfitAndLossComponent } from '../sales/trip-profit-and-loss/trip-profit-and-loss.component';
 
 const routes: Routes = [
    { path: 'tran-register', component: TranRegisterComponent ,canActivate:[ReportGuard]},
    { path: 'pnl-report', component: PlreportsComponent ,canActivate:[ReportGuard]},
    { path: 'cash-balance', component: CashBalancesComponent ,canActivate:[ReportGuard]},
-   {path:'client-balance',component:TripProfitAndLossComponent}
 ];
 
 @NgModule({
